2259a6ecdb1c5814d4d2122c1004be94c46d8fec
gitarecy <gitarecynthia@gmail.com>
2023-10-09 16:51:57 +0200

    improvement-1

--- src/improvement-1.js
+++ src/improvement-1.js
@@ -5,12 +5,28 @@
  */
 
 // FUNCTION 1
-export default function cntRbt(ls) {
-  var cnt = 0;
-  for (var i = 0; i < ls.length; i++) {
-    if (ls[i] == "R") {
-      cnt++;
-    }
+export default function countOfRsInArray(ls) {
+  //changed all vars to let
+  // let count = 0;
+  // for (let i = 0; i < ls.length; i++) {
+  //   if (ls[i] == "R") {
+  //     count++;
+  //   }
+  // }
+  //searching for R globally
+  const regex = /R/g;
+  if (typeof ls === "string") {
+    const count = ls.match(regex).length;
+    return count;
+  } else if (typeof ls === "object") {
+    const count = ls.join("").match(regex).length;
+    return count;
   }
-  return cnt;
 }
+
+// const sl = "Robert";
+// const ls = ["R", "G", "B", "R", "R"];
+// console.log(countOfRsInArray(ls));
+// console.log(countOfRsInArray(sl));
+
+// export default cntRbt;
