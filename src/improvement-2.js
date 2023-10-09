f18de665ec2c13f6d40c21463ce638c1bbfc1179
gitarecy <gitarecynthia@gmail.com>
2023-10-09 17:12:25 +0200

    improvement-2

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -5,13 +5,21 @@
  */
 
 // FUNCTION 2
-export default function processItems(items) {
-  const processedItems = [];
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    if (item.type === "Food") {
-      processedItems.push(item);
-    }
-  }
-  return processedItems;
+export default function findFoodItems(items) {
+  // const processedItems = [];
+  // for (let i = 0; i < items.length; i++) {
+  //   const item = items[i];
+  //   if (item.type === "Food") {
+  //     processedItems.push(item);
+  //   }
+  // }
+  const arr = items.filter((item) => item.type === "Food");
+  return arr;
 }
+
+// const items = [
+//   { type: "Food", name: "Apple" },
+//   { type: "Drink", name: "Water" },
+//   { type: "Food", name: "Banana" },
+// ];
+// console.log(findFoodItems(items));
