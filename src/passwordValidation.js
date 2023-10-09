93f8176323c0d6898f4d1b649fa4f3a0bce0947f
gitarecy <gitarecynthia@gmail.com>
2023-10-09 22:01:28 +0200

    password validation 90% tests passed

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -20,7 +20,24 @@ export default function isValidPassword(password = "") {
    *   return ...;
    * }
    */
+  // Check if the password is at least 10 characters long and at most 20 characters long.
+  if (password.length < 10 || password.length > 20) return false;
+
+  // Check if the password is composed of at least 4 different characters.
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
+
+  // Check if the password contains at least one lowercase letter, one uppercase letter, and one number.
+  const regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*/;
+  if (!regex.test(password)) return false;
+
+  // Check if the password does not contain any special characters.
+  const specialCharactersRegex = /[\W]/;
+  if (specialCharactersRegex.test(password)) return false;
+
+  // Check if the password is not in the list of forbidden passwords.
+  if (forbiddenPasswords.includes(password)) return false;
+
+  // The password is valid.
   return true;
 }
