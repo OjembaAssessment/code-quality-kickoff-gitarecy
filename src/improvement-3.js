554bca1aa8e4d22707d68e062de4e9023e88f6fd
gitarecy <gitarecynthia@gmail.com>
2023-10-09 16:10:43 +0200

    improvement-3 correction

--- src/improvement-3.js
+++ src/improvement-3.js
@@ -6,11 +6,17 @@
 
 // FUNCTION 3
 export default function findLargestNumber(numbers) {
-  let largest = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-      largest = numbers[i];
-    }
-  }
-  return largest;
+  // let largest = numbers[0];
+  // for (let i = 1; i < numbers.length; i++) {
+  //   if (numbers[i] > largest) {
+  //     largest = numbers[i];
+  //   }
+  // }
+  //return largest;
+  return Math.max(...numbers);
 }
+
+// const arr = [2, 6, 8, 9, 10];
+// console.log(findLargestNumber(arr));
+
+//export default findLargestNumber;
