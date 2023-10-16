00924f7cd8660f566ad73e5cfc78938429177a29
gitarecy <gitarecynthia@gmail.com>
2023-10-16 14:57:14 +0200

    password validation, all tests

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -6,38 +6,57 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @param {string} password
  * @returns {boolean}
  */
+function sequence(password) {
+  const sequence = "0123456789876543210";
+  const matches = password.match(/(\d){4}/g);
+  return matches?.some((match) => sequence.includes(match));
+}
+//DONT FORGET THIS!!!! export default
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
-
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
-  // Check if the password is at least 10 characters long and at most 20 characters long.
-  if (password.length < 10 || password.length > 20) return false;
-
-  // Check if the password is composed of at least 4 different characters.
+  //Have at least 4 different digits/characters
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
+  //Be between 8 and 15 characters long
+  if (password.length !== 10) return false;
+  //Contain at least one number
+  const checkingForDigit = /[0-9]/;
+  if (!checkingForDigit.test(password)) return false;
+  //Contain at least one uppercase letter
+  const checkingLowercase = /[a-z]/;
+  if (!checkingLowercase.test(password)) return false;
 
-  // Check if the password contains at least one lowercase letter, one uppercase letter, and one number.
-  const regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*/;
-  if (!regex.test(password)) return false;
+  //Contain at least one lowercase letter
+  const checkingUppercase = /[A-Z]/;
+  if (!checkingUppercase.test(password)) return false;
 
-  // Check if the password does not contain any special characters.
-  const specialCharactersRegex = /[\W]/;
-  if (specialCharactersRegex.test(password)) return false;
+  //Not contain any special characters
+  const alphaNumerics = /^[a-zA-Z0-9]+$/;
+  if (!alphaNumerics.test(password)) return false;
 
-  // Check if the password is not in the list of forbidden passwords.
+  //Not contain any forbidden passwords
   if (forbiddenPasswords.includes(password)) return false;
 
-  // The password is valid.
+  //Not contain a directly ascending/descending sequence of numbers
+
+  if (sequence(password)) return false;
+
+  // No ascending/descending sequence found
   return true;
 }
+
+//////////////////////////////////////
+
+// The password is valid.
+
+// * * * YOUR CODE GOES IN HERE ... * * *
+/*
+ * if (password is not exactly 10 digits or characters...) {
+ *   return ...;
+ * }
+ *
+ * if (is not composed by digits and numbers) {
+ *   return ...;
+ * }
+ */
