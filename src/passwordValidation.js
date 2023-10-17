9e8f3801cb0066316e8e3263b072a02a954c8e75
gitarecy <gitarecynthia@gmail.com>
2023-10-17 11:41:47 +0200

    adding more descriptions and positioning returns in better order

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -6,6 +6,8 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @param {string} password
  * @returns {boolean}
  */
+
+//checks if sequence is ascending or descending
 function sequence(password) {
   const sequence = "0123456789876543210";
   const matches = password.match(/(\d){4}/g);
@@ -15,11 +17,13 @@ function sequence(password) {
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
-  //Have at least 4 different digits/characters
-  const setOfPassword = new Set([...password]);
-  if (setOfPassword.size < 4) return false;
+  //Not contain any forbidden passwords
+  if (forbiddenPasswords.includes(password)) return false;
   //Be between 8 and 15 characters long
   if (password.length !== 10) return false;
+  //Not contain any special characters
+  const alphaNumerics = /^[a-zA-Z0-9]+$/;
+  if (!alphaNumerics.test(password)) return false;
   //Contain at least one number
   const checkingForDigit = /[0-9]/;
   if (!checkingForDigit.test(password)) return false;
@@ -30,14 +34,9 @@ export default function isValidPassword(password = "") {
   //Contain at least one lowercase letter
   const checkingUppercase = /[A-Z]/;
   if (!checkingUppercase.test(password)) return false;
-
-  //Not contain any special characters
-  const alphaNumerics = /^[a-zA-Z0-9]+$/;
-  if (!alphaNumerics.test(password)) return false;
-
-  //Not contain any forbidden passwords
-  if (forbiddenPasswords.includes(password)) return false;
-
+  //Have at least 4 different digits/characters
+  const setOfPassword = new Set([...password]);
+  if (setOfPassword.size < 4) return false;
   //Not contain a directly ascending/descending sequence of numbers
 
   if (sequence(password)) return false;
