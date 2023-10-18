4def2fb44205b176e7cdb5414a9b456b9fd2f4e5
gitarecy <gitarecynthia@gmail.com>
2023-10-18 11:36:14 +0200

    refactor

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -7,30 +7,30 @@
  */
 
 export default function penaltyPoints(password = "") {
-  //check if password is not empty
   if (!password) return 0;
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
+
+  // Ensure that password is always a string.
   if (typeof password !== "string") password = String(password);
-  //matching all sequences of two or more identical characters in the password string.
+
+  // Match all sequences of two or more identical characters in the password string.
   const matches = password.match(/(.)\1+/g);
   if (!matches) return 0;
-  // If there are no matches, return 0.
+
   if (matches.length === 0) return 0;
-  // Calculate the penalty points for each match.
-  const penaltyPoints = matches
-    .map((e) => e.length)
-    .map((e) => {
-      let count = 0;
-      if (e === 2) {
-        // For a sequence of two identical characters, add 1 penalty point.
-        count = count + 1;
-      } else if (e > 2) {
-        // For a sequence of three or more identical characters, add 2 penalty points.
-        count = count + 2;
-      }
-      return count;
-    })
-    .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  // Calculate the penalty points for each match using the reduce() method.
+  const penaltyPoints = matches.reduce((sum, currentValue) => {
+    const length = currentValue.length;
+    // Calculate the penalty points for the match.
+    let count = 0;
+    if (length === 2) {
+      count += 1;
+    } else if (length > 2) {
+      count += 2;
+    }
+    return (sum += count);
+  }, 0);
+
   // Return the sum of penalty points.
   return penaltyPoints;
 }
